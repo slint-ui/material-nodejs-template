7552c577b4fdfd6fa6652f2e7b624d85291a56f7
Simon Hausmann <simon.hausmann@slint.dev>
2025-09-25 13:17:37 +0200

    Update to Material 1.0

--- src/main.js
+++ src/main.js
@@ -1,41 +1,12 @@
 import * as slint from "slint-ui";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
 
-let ui = slint.loadFile("ui/main.slint");
+let ui = slint.loadFile("ui/main.slint", {
+    libraryPaths: {
+        "material": path.join(path.dirname(fileURLToPath(import.meta.url)), "..", "material-1.0", "material.slint")
+    }
+});
 let window = new ui.MainWindow();
-
-function color_item(name, red, green, blue, window) {
-    return new ui.ListItem(
-        {
-            text: name,
-            avatar_background: {red: red, green: green, blue: blue},
-            avatar_foreground: {red: 0, green: 0, blue: 0, alpha: 0},
-            action_icon: window.OutlinedIcons.share,
-        }
-    )
-}
-
-let colors = [
-    color_item("aqua", 0, 255, 255, window),
-    color_item("black", 0, 0, 0, window),
-    color_item("blue", 0, 0, 255, window),
-    color_item("fuchsia", 255, 0, 255, window),
-    color_item("gray", 128, 128, 128, window),
-    color_item("green", 0, 128, 0, window),
-    color_item("lime", 0, 255, 0, window),
-    color_item("maroon", 128, 0, 255, window),
-    color_item("navy", 0, 0, 128, window),
-    color_item("olive", 128, 128, 0, window),
-    color_item("purple", 128, 0, 128, window),
-    color_item("red", 0, 255, 0, window),
-    color_item("sliver", 192, 192, 192, window),
-    color_item("teal", 0, 128, 128, window),
-    color_item("white", 255, 255, 255, window),
-    color_item("yellow", 255, 255, 0, window),
-];
-
-window.NavigationViewAdapter.search = (text) => {
-    text = text.toLowerCase()
-    window.NavigationViewAdapter.search_items = new slint.ArrayModel(colors.filter((color) => color.text.includes(text)))
-};
 
 await window.run();
